c0f570012c2fa1105dcd632784b038270728c711
Jean Luca Bez <jeanlucabez@gmail.com>
2022-11-14 10:00:33 -0600

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,10 @@
+#SBATCH --job-name=ppm
+#SBATCH --qos=debug
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --constraint=haswell
+#SBATCH -o ppm-%J.out
+#SBATCH -e ppm-%J.err
+#SBATCH --reservation=marathon
+
+/usr/bin/time --format='%e' timeout 5m ./A < input.txt > output.out
